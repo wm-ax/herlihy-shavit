124e3869c7b7c05e3eda142802649796bd67e53d
Max Weiss <30f0fn@gmail.com>
2020-02-04 10:38:17 -0500

    add reporting of states

--- src/1/philosophers/src/lib.rs
+++ src/1/philosophers/src/lib.rs
@@ -1,18 +1,21 @@
 // 2840
 use std::sync::Arc;
 use std::thread;
-use rand::rngs::ThreadRng;
-// use rand::distributions::Uniform;
-use rand::{Rng};
-use try_lock::TryLock;
+use std::sync::mpsc::{Sender};
+use std::sync::mpsc;
 use std::time;
 use std::sync::atomic::{AtomicBool, Ordering};
+use rand::rngs::ThreadRng;
+use rand::{Rng};
+use try_lock::TryLock;
 
+// starvation-free version
 
 const TABLE_SIZE: usize = 5;
 
 
 pub fn dine(duration: u64) {
+    let mut state_receivers = Vec::new();
     let chopsticks = (0..TABLE_SIZE).map(
         |_| Arc::new(TryLock::new(Chopstick))
     ).collect::<Vec<_>>();
@@ -21,13 +24,21 @@ pub fn dine(duration: u64) {
         let chopsticks = [chopsticks[id].clone(),
                           chopsticks[(id + 1) % TABLE_SIZE].clone()];
         let stopped = stopped.clone();
+        let (sender, receiver) = mpsc::channel();
+        state_receivers.push(receiver);
         thread::spawn(|| {
-            let mut philosopher = Philosopher::new(chopsticks, stopped);
-            // println!("initialized a philosopher"); 
+            let mut philosopher = Philosopher::new(chopsticks, stopped, sender);
             philosopher.dine();
         })
     }).collect();
-    thread::sleep(time::Duration::from_millis(duration));
+    let num_checks = 10;
+    for i in 0 .. num_checks {
+        thread::sleep(time::Duration::from_millis(duration / num_checks));
+        print!("Report {}: their states are ", i);
+        let states : Vec<_> = state_receivers.iter().map(
+            |rcv| rcv.try_iter().last().unwrap()).collect();
+        println!("{:?}", states);
+    }
     stopped.store(true, Ordering::SeqCst);
     for thr in philosophers {
         thr.join().unwrap();
@@ -39,6 +50,7 @@ struct Philosopher {
     chopsticks: [Arc<TryLock<Chopstick>>; 2],
     state: State,
     timer: Timer,
+    sender: Sender<State>,
     amount_eaten: u64,
     stopped: Arc<AtomicBool>,
 }
@@ -47,18 +59,19 @@ struct Philosopher {
 impl Philosopher {
 
     fn new(chopsticks: [Arc<TryLock<Chopstick>>; 2],
-           stopped: Arc<AtomicBool>) -> Self {
+           stopped: Arc<AtomicBool>,
+           sender: Sender<State>) -> Self {
         Philosopher {
             state: State::Thinking,
             chopsticks: chopsticks.clone(),
             timer: Timer::new(),
+            sender: sender,
             amount_eaten: 0,
             stopped: stopped.clone(),
         }
     }
 
     pub fn dine(&mut self) {
-        println!("philosopher on {:?} is dining", thread::current().id());
         while !self.is_done() {
             match self.state {
                 State::Eating => 
@@ -70,7 +83,7 @@ impl Philosopher {
                     self.try_to_eat()
             }
         }
-        println!("Philosopher on {:?} done, having eaten {} grains of rice",
+        println!("Philosopher {:?} done, having eaten {} grains of rice",
                  thread::current().id(),
                  self.amount_eaten);
     }
@@ -84,6 +97,7 @@ impl Philosopher {
         match chopsticks {
             Some(_) => {
                 self.state = State::Eating;
+                self.sender.send(self.state).unwrap();
                 let amount = self.timer.sleep_random();
                 self.amount_eaten += amount;
             },
@@ -93,11 +107,13 @@ impl Philosopher {
 
     fn think(&mut self) {
         self.state = State::Thinking;
+        self.sender.send(self.state).unwrap();
         self.timer.sleep_random();
     }
 
     fn wait(&mut self) {
         self.state = State::Waiting;
+        self.sender.send(self.state).unwrap();
     }
 
     fn is_done(&self) -> bool {
@@ -127,6 +143,7 @@ impl Timer {
 }
 
 
+#[derive(Debug, Copy, Clone)]
 enum State {
     Eating,
     Thinking,
